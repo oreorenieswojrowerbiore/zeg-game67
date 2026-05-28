caa4ef4dce1de4357e93cbe85f15030c10edf8c8
alanekkrulnalstolatek-art <alanekkrulnalstolatek@gmail.com>
2026-05-28 09:52:46 +0200

    zmiana ruchu gracza

--- js/player.js
+++ js/player.js
@@ -1,18 +1,28 @@
-const player = {
-  x: 1,
-  y: 1,
-  hp: 3,
-  keys: 0
-};
-
 function movePlayer(dx, dy) {
 
   const newX = player.x + dx;
   const newY = player.y + dy;
 
+  // ZABEZPIECZENIE MAPY
+  if (
+    newY < 0 ||
+    newY >= levelMap.length ||
+    newX < 0 ||
+    newX >= levelMap[newY].length
+  ) {
+    return;
+  }
+
   const tile = levelMap[newY][newX];
 
+  // ŚCIANA
   if (tile === "#") {
+    return;
+  }
+
+  // EXIT WYMAGA KLUCZA
+  if (tile === "E" && player.keys <= 0) {
+    alert("Potrzebujesz klucza!");
     return;
   }
 
